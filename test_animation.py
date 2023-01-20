ffa05f3299774bf3294303f8a8c30d2b6ce6b93b
Tori Wuthrich <tori.wuthrich@wahoofitness.com>
2023-01-20 11:57:07 -0500

    Finish dummy visualization

--- test_animation.py
+++ test_animation.py
@@ -34,17 +34,24 @@ from matplotlib import animation
 
 distance = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 elevation = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-target = [6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
-
+target = [15, 15, 15, 15, 15, 15, 15, 15, 15, 15]
+est = [13, 14, 15, 16, 17, 13, 14, 15, 16, 17]
+time = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 fig, ax = plt.subplots(figsize=(5, 5))
+ax2 = ax.twinx()
 
 ax.set_ylim([0, 11])
 ax.set_xlim([0, 11])
 
-(elevation_line,) = ax.plot([], [], color="red", lw=2)
-(distance_line,) = ax.plot([], [], color="blue", lw=2)
-(target_line,) = ax.plot(elevation, target, color="green", lw=2)
+ax2.set_ylim([10, 20])
+ax2.set_xlim([0, 11])
+
+(elevation_line,) = ax.plot(time, elevation, color="orange", lw=2)
+(locator_dot,) = ax.plot(np.nan, np.nan, marker="o", color="blue")
+
+(target_line,) = ax2.plot(elevation, target, color="green", lw=2)
+(time_line,) = ax2.plot([], [], color="red", lw=2)
 
 
 def animate_func(i):
@@ -52,16 +59,16 @@ def animate_func(i):
     # ax.set_xlim([0, 11])
     # ax.set_ylim([0, 11])
     # ax.scatter(distance[0:i], elevation[0:i])
-    elevation_line.set_data(distance[0 : i + 1], elevation[0 : i + 1])
-    distance_line.set_data(distance[0 : i + 1], distance[0 : i + 1])
-    return elevation_line, distance_line
+    locator_dot.set_data(time[i], elevation[i])
+    time_line.set_data(time[0 : i + 1], est[0 : i + 1])
+    return locator_dot, time_line
 
 
 def init_fun():
 
-    elevation_line.set_data([], [])
-    distance_line.set_data([], [])
-    return elevation_line, distance_line
+    locator_dot.set_data([], [])
+    time_line.set_data(np.nan, np.nan)
+    return locator_dot, time_line
 
 
 # ax.plot(distance, elevation, label="Elevation Profile")
